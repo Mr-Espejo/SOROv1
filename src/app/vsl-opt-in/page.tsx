b1d61e263bb3bf77475b3ea0ed8d10bebc6ed97a
Nella Buffet <nelasbuffetbog@gmail.com>
2025-11-06 21:43:51 +0000

    cambialo a aeste (https://file.notion.so/f/f/ed6477b8-cb1b-4174-8d7a-3c8

--- src/app/vsl-opt-in/page.tsx
+++ src/app/vsl-opt-in/page.tsx
@@ -71,7 +71,7 @@ export default function VslOptInPage() {
             <div className="w-full max-w-4xl">
               <AspectRatio ratio={16 / 9} className="overflow-hidden rounded-xl shadow-2xl bg-black">
                 <video
-                  src="https://file.notion.so/f/f/ed6477b8-cb1b-4174-8d7a-3c87e939e5b9/1f137d7d-5d61-4001-909e-c0a1880f8826/Stan_Christ_-_Trepidation.webm?table=block&id=2995454f-b3d8-80cf-9dca-ff1955cf0f58&spaceId=ed6477b8-cb1b-4174-8d7a-3c87e939e5b9&expirationTimestamp=1762488000000&signature=Z3Qw3TnUAp4Bkqcr3y5ITV3Mq7fP47UOfTShcdvq3ws&downloadName=Stan+Christ+-+Trepidation.webm"
+                  src="https://file.notion.so/f/f/ed6477b8-cb1b-4174-8d7a-3c87e939e5b9/f969228a-d764-42b6-84cd-f75b4eec0e85/1106_(1).mp4?table=block&id=2a35454f-b3d8-80cf-9116-e5816446e804&spaceId=ed6477b8-cb1b-4174-8d7a-3c87e939e5b9&expirationTimestamp=1762488000000&signature=PqTcfWQPHosNhjBJuIcz-_lia2-wqGmzQE5nOV9Q9zU&downloadName=1106+%281%29.mp4"
                   controls
                   className="h-full w-full"
                 >
