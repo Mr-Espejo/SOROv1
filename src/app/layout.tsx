f2ccf9319c4d13954157a96c5b9582b39b41f0bc
Nella Buffet <nelasbuffetbog@gmail.com>
2025-11-24 00:54:30 +0000

    Identificador 818538310887510

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,14 +54,14 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', 'TU_PIXEL_ID');
+            fbq('init', '818538310887510');
             fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
           {/* Fallback para navegadores sin JavaScript. Reemplaza TU_PIXEL_ID aquí también. */}
           <img height="1" width="1" style={{display: 'none'}}
-               src="https://www.facebook.com/tr?id=TU_PIXEL_ID&ev=PageView&noscript=1" />
+               src="https://www.facebook.com/tr?id=818538310887510&ev=PageView&noscript=1" />
         </noscript>
         {/* --- Fin del Píxel de Seguimiento --- */}
       </body>
